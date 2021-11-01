76cd14e62a1ed23d772c468148d5fe9ab101096b
csachy13 <80550433+csachy13@users.noreply.github.com>
2021-11-01 10:07:15 -0400

    Add files via upload

---
+++ yums.js
@@ -0,0 +1,30 @@
+class Yums {
+    constructor() {
+    }
+    
+    getInfo() {
+        return {
+            "id": "Yum",
+            "name": "Yum",
+            "blocks": [
+                {
+                    "opcode": "addTwo",
+                    "blockType": "reporter",
+                    "text": "2 + [str]",
+                    "arguments": {
+                        "str": {
+                            "type": "string",
+                            "defaultValue": "Hello , World!"
+                        },
+                    }
+                },
+            ]
+        }
+    }
+
+    /* add methods for blocks */
+}
+addTwo({str}) {
+    return str + 2
+}
+Scratch.extensions.register(new Yums())
